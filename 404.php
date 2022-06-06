3d4f127c19095b24757160443b106f7fe021a316
Harry-Gardiner <gardiner_harry@yahoo.com>
2022-06-06 11:01:21 +0100

    Posts Layout - Created blog template page. Also added some content to 404 taken from blog

--- 404.php
+++ 404.php
@@ -13,7 +13,7 @@ get_header();
 
 <main id="primary" class="site-main">
 
-	<section class="error-404 not-found container">
+	<section class="error-404 not-found container margin">
 		<header class="page-header">
 			<h1 class="page-title"><?php esc_html_e('Oops! That page can&rsquo;t be found.', 'wrhs'); ?></h1>
 		</header><!-- .page-header -->
@@ -21,7 +21,32 @@ get_header();
 		<div class="page-content">
 			<p><?php esc_html_e('It looks like nothing was found at this location. Maybe try one of the links below or a search?', 'wrhs'); ?></p>
 
-
+			<div class="archive-sidebar">
+				<div class="archive-categories">
+					<p><strong><?php echo esc_html__('Categories', 'wrhs'); ?></strong></p>
+					<ul class="category-list">
+						<?php wp_list_categories(
+							array(
+								'title_li' => '',
+								'hide_title_if_empty' => true
+							)
+						); ?>
+					</ul>
+				</div>
+				<div class="archive-tags">
+					<p><strong><?php echo esc_html__('Tags', 'wrhs'); ?></strong></p>
+					<?php wp_tag_cloud(); ?>
+				</div>
+				<div class="archive-authors">
+					<p><strong><?php echo esc_html__('Authors', 'wrhs'); ?></strong></p>
+					<?php wp_list_authors(
+						array(
+							'hide_empty' => 'true',
+							'optioncount' => 'true'
+						)
+					); ?>
+				</div>
+			</div>
 
 		</div><!-- .page-content -->
 	</section><!-- .error-404 -->
