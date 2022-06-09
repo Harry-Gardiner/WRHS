becffa11630e1f3f82b82e29bcec31e6cf30a3ec
Harry-Gardiner <gardiner_harry@yahoo.com>
2022-06-09 14:22:50 +0100

    PDFs - restricted pdf image size

--- partials/common/featured-pdf.php
+++ partials/common/featured-pdf.php
@@ -2,14 +2,14 @@
 $doc_id = get_the_ID();
 
 if (has_post_thumbnail()) {
-    $image = the_post_thumbnail();
+    $image = get_the_post_thumbnail_url();
 } else {
     $image = get_template_directory_uri() . '/images/pdf-placeholder.png';
 }
 ?>
 <div class="pdfs__card">
 
-    <a href="<?php the_permalink(); ?>" class="pdfs__card__link pb-2 mb-4" title="Permanent Link to <?php the_title_attribute(); ?>">
+    <a href="<?php the_permalink(); ?>" class="pdfs__card__link pt-2 pb-2 mb-4" title="Permanent Link to <?php the_title_attribute(); ?>">
         <img class="pdfs__card__image" src="<?php echo esc_url($image) ?>" alt="">
         <h4 class="pdfs__card__title mt-2 mb-0"><?php the_title(); ?></h4>
     </a>
