206a2fb9e9d08a6f47b6e024236ac3528ce77456
Harry-Gardiner <gardiner_harry@yahoo.com>
2022-06-13 10:48:46 +0100

    Products - created a custom products page as wasnt having much luck over-riding woo default

---
+++ template-products.php
@@ -0,0 +1,13 @@
+<?php
+/* Template Name: Custom Products Page */
+get_header();
+?>
+<main id="main" class="contact container">
+    <?php the_title('<h1 class="entry-title">', '</h1>'); ?>
+    <?php the_content() ?>
+
+    PRODUCTS GO HERE
+
+</main>
+<?php
+get_footer();
