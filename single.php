4b23f1ec24888a80a352cad808ae730b991dafb1
Harry-Gardiner <gardiner_harry@yahoo.com>
2022-08-19 16:09:06 +0100

    Next/Prev wont show for single documents. Prevent people reaching protected documents

--- single.php
+++ single.php
@@ -19,16 +19,18 @@ get_header();
 
 		get_template_part('template-parts/content', get_post_type());
 
-		the_post_navigation(
-			array(
-				'prev_text' => '<span class="nav-subtitle">' . esc_html__('Previous:', 'wrhs') . '</span> <span class="nav-title">%title</span>',
-				'next_text' => '<span class="nav-subtitle">' . esc_html__('Next:', 'wrhs') . '</span> <span class="nav-title">%title</span>',
-				'excluded_terms' => (current_user_can('upload_files') ? null : 53)
-			)
-		);
+		if (!is_singular('document')) {
+			the_post_navigation(
+				array(
+					'prev_text' => '<span class="nav-subtitle">' . esc_html__('Previous:', 'wrhs') . '</span> <span class="nav-title">%title</span>',
+					'next_text' => '<span class="nav-subtitle">' . esc_html__('Next:', 'wrhs') . '</span> <span class="nav-title">%title</span>',
+					'excluded_terms' => (current_user_can('upload_files') ? null : 53)
+				)
+			);
 
-		echo '<hr class="hr-gray">';
 
+			echo '<hr class="hr-gray">';
+		}
 		// If comments are open or we have at least one comment, load up the comment template.
 		if (comments_open() || get_comments_number()) :
 			comments_template();
