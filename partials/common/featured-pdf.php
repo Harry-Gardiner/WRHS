df6bcc51891eb3695c7349c66b198b4e35dad238
Harry-Gardiner <gardiner_harry@yahoo.com>
2022-08-19 16:11:53 +0100

    Documents download now triggers download as a pdf not .html

--- partials/common/featured-pdf.php
+++ partials/common/featured-pdf.php
@@ -1,6 +1,6 @@
 <?php
 $doc_id = get_the_ID();
-
+$pdf_document = get_field('wr_add_document')['url'];
 if (has_post_thumbnail()) {
     $image = get_the_post_thumbnail_url();
 } else {
@@ -13,7 +13,7 @@ if (has_post_thumbnail()) {
         <img class="pdfs__card__image" src="<?php echo esc_url($image) ?>" alt="">
         <h4 class="pdfs__card__title mt-2 mb-0"><?php the_title(); ?></h4>
     </a>
-    <a href="<?php the_permalink(); ?>" download="<?php the_title_attribute(); ?>">
+    <a href="<?php echo $pdf_document ?>" download="<?php the_title_attribute(); ?>">
         <button class="btn btn--download mb-4">Download</button>
     </a>
 </div>
